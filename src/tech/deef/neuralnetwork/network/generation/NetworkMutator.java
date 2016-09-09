ec5fbe6c1b75bbd8d3a4d06c8224f6dad2d499a5
deef0000dragon1 <deef551@gmail.com>
2016-09-09 14:38:15 -0500

    add calculated values to finals in calcNumNodesToAdd
    also removes excess code used to calculate values in first place

--- src/tech/deef/neuralnetwork/network/generation/NetworkMutator.java
+++ src/tech/deef/neuralnetwork/network/generation/NetworkMutator.java
@@ -113,8 +113,11 @@ public class NetworkMutator {
 		return null;
 	}
 	
+	private final static double MUTATOR = 0.2830554;
+	private final static double MULTIPLIER = 0.7105625;
+	private final static double ADDER = 0.1744375;
 	private int calculateNumberOfNodesToAdd(int numberOfNodes, double mutatorValue, Random rand){
-		double powerFunctionOutput = (Math.pow(mutatorValue, .05))*0.615+.27; //calculates the power of the function
+		double powerFunctionOutput = (Math.pow(mutatorValue, MUTATOR))*MULTIPLIER+ADDER; //calculates the power of the function
 		double randomizerVal = (((rand.nextDouble()-0.5)*2) / 10) * powerFunctionOutput; //calculates the random power mutation
 		return (int) Math.pow(numberOfNodes, (powerFunctionOutput + randomizerVal)); // calculates the total number of nodes to add
 		
